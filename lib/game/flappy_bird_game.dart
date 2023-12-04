c0425552aa3961daf7bdd90a59bcb62922016d9e
EhsanTgv <EhsanTgv@gmail.com>
2023-12-04 17:51:42 +0300

    Implement loop for pipes

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,11 +1,16 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flappy_bird/components/background.dart';
 import 'package:flappy_bird/components/bird.dart';
+import 'package:flappy_bird/components/configuration.dart';
 import 'package:flappy_bird/components/ground.dart';
 import 'package:flappy_bird/components/pipe_group.dart';
 
 class FlappyBirdGame extends FlameGame {
+  FlappyBirdGame();
+
   late Bird bird;
+  Timer interval = Timer(Config.pipInterval, repeat: true);
 
   @override
   Future<void> onLoad() async {
@@ -13,7 +18,14 @@ class FlappyBirdGame extends FlameGame {
       Background(),
       Ground(),
       bird = Bird(),
-      PipeGroup(),
     ]);
+
+    interval.onTick = () => add(PipeGroup());
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    interval.update(dt);
   }
 }
