9badc5472ae7f571a772fcfbe271224caf7dc283
EhsanTgv <EhsanTgv@gmail.com>
2023-12-05 16:53:51 +0300

    Implement MainMenu and GameOver Screens

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -12,6 +12,7 @@ class FlappyBirdGame extends FlameGame with TapDetector, HasCollisionDetection {
 
   late Bird bird;
   Timer interval = Timer(Config.pipInterval, repeat: true);
+  bool isHit = false;
 
   @override
   Future<void> onLoad() async {
